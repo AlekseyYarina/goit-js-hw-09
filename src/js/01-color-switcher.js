df406cc775edd95b66ed78534647558021d044e8
AlekseyYarina <aleksey.yarina@gmail.com>
2023-10-26 21:54:14 +0300

    upd task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,7 @@
 const randomColor = function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 };
 
 const refs = {
@@ -10,7 +10,7 @@ const refs = {
   stopBtn: document.querySelector('[data-stop]'),
 };
 let isActive = false;
-refs.stopBtn.setAttribute('disabled', "")
+refs.stopBtn.disabled = true; // Установка кнопки как неактивной
 
 refs.startBtn.addEventListener('click', () => {
   if (isActive) {
@@ -18,16 +18,16 @@ refs.startBtn.addEventListener('click', () => {
   }
   timerId = setInterval(() => {
     refs.bodyColor.style.backgroundColor = randomColor();
-    isActive = true
-    refs.startBtn.setAttribute('disabled', "")
-    refs.stopBtn.removeAttribute('disabled')
+    isActive = true;
+    refs.startBtn.disabled = true; // Установка кнопки "Start" как неактивной
+    refs.stopBtn.disabled = false; // Удаление неактивного состояния кнопки "Stop"
   }, 1000);
 });
 
 refs.stopBtn.addEventListener('click', () => {
   clearInterval(timerId);
   isActive = false;
-  refs.startBtn.removeAttribute('disabled')
-  refs.stopBtn.setAttribute('disabled', "")
-  console.log(`Interval with id ${timerId} has stopped!`);
+  refs.startBtn.disabled = false; // Удаление неактивного состояния кнопки "Start"
+  refs.stopBtn.disabled = true; // Установка кнопки "Stop" как неактивной
+  console.log(`Интервал с идентификатором ${timerId} остановлен!`);
 });
