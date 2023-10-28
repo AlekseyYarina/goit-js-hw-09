26eeab3171477144d39ff4c9813791933009b750
AlekseyYarina <aleksey.yarina@gmail.com>
2023-10-28 11:14:18 +0300

    add object refs task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,17 +1,19 @@
 import Notiflix from 'notiflix';
 
-const form = document.querySelector('.form');
+const refs = {
+form: document.querySelector('.form'),
+delayInput: form.querySelector('input[name="delay"]'),
+stepInput: form.querySelector('input[name="step"]'),
+amountInput: form.querySelector('input[name="amount"]'),
+}
 
-form.addEventListener('submit', function (e) {
+
+const delay = parseInt(refs.delayInput.value);
+const step = parseInt(refs.stepInput.value);
+const amount = parseInt(refs.amountInput.value);
+
+refs.form.addEventListener('submit', function (e) {
   e.preventDefault();
-
-  const delayInput = form.querySelector('input[name="delay"]');
-  const stepInput = form.querySelector('input[name="step"]');
-  const amountInput = form.querySelector('input[name="amount"]');
-
-  const delay = parseInt(delayInput.value);
-  const step = parseInt(stepInput.value);
-  const amount = parseInt(amountInput.value);
 
   for (let i = 1; i <= amount; i += 1) {
     createPromise(i, delay + (i - 1) * step)
